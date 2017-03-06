1a492c0db89c61ab82390c83d7e5c6f8711bb9fe
eli-zarrabal <elizabethzarrabal@gmail.com>
2017-03-05 19:36:37 -0600

    calcu

--- app/src/main/java/itsco/edu/calculadora/MainActivity.java
+++ app/src/main/java/itsco/edu/calculadora/MainActivity.java
@@ -1,15 +1,12 @@
 package itsco.edu.calculadora;
 
 
-
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.View;
 import android.widget.TextView;
 
-
-public class MainActivity extends Activity{
-
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,131 +14,144 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
     }
 
-    Double n1,n2,resultado;
+
+    int contador = 0;
+
+    Double numero1, numero2, resultado;
     String operador;
 
-    public void onClickIgual(View miView)
-    {
-        TextView res= (TextView) findViewById(R.id.txt_resultado) ;
-        n2 = Double.parseDouble(res.getText().toString());
+    public void onClickIgual(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        numero2 = Double.parseDouble(tv.getText().toString());
 
-        if(operador.equals("+"))
-        {
-            resultado= n1+n2;
+        if (operador.equals("+")) {
+            resultado = numero1 + numero2;
+        } else if (operador.equals("-")) {
+            resultado = numero1 - numero2;
+        } else if (operador.equals("*")) {
+            resultado = numero1 * numero2;
+        } else if (operador.equals("/")) {
+            resultado = numero1 / numero2;
         }
-        else if(operador.equals("-"))
-        {
-            resultado= n1-n2;
+        tv.setText(resultado.toString());
+    }
+
+    public void onClickSuma(View miView) {
+        operador = "+";
+        onClickOperacionCapturaNumero1(miView);
+
+        contador=0;
+    }
+
+    public void onClickResta(View miView) {
+        operador = "-";
+        onClickOperacionCapturaNumero1(miView);
+
+        contador=0;
+    }
+
+    public void onClickMultiplicacion(View miView) {
+        operador = "*";
+        onClickOperacionCapturaNumero1(miView);
+
+        contador=0;
+    }
+
+    public void onClickDivision(View miView) {
+        operador = "/";
+        onClickOperacionCapturaNumero1(miView);
+
+        contador=0;
+    }
+
+    public void onClickOperacionCapturaNumero1(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        numero1 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+    public void onClickBorrar(View miView) {
+        numero1 = 0.0;
+        numero2 = 0.0;
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText("");
+        contador=0;
+    }
+
+    public void onClickBtn1(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "1");
+    }
+
+    public void onClickBtn2(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "2");
+    }
+
+    public void onClickBtn3(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "3");
+    }
+
+    public void onClickBtn4(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "4");
+    }
+
+    public void onClickBtn5(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "5");
+    }
+
+    public void onClickBtn6(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "6");
+    }
+
+    public void onClickBtn7(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "7");
+    }
+
+    public void onClickBtn8(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "8");
+    }
+
+    public void onClickBtn99(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "9");
+    }
+
+    public void onClickBtn0(View miView) {
+        TextView tv = (TextView) findViewById(R.id.txt_res);
+        tv.setText(tv.getText() + "0");
+    }
+
+    public void onClickBtnPunto(View miView) {
+
+
+        if (contador == 0) {
+
+            TextView tv = (TextView) findViewById(R.id.txt_res);
+            tv.setText(tv.getText() + ".");
+            operador=(tv.getText().toString());
+
+            if(operador.toString().equals(".")){
+
+                tv.setText("");
+                tv.setText(tv.getText()+ "0.");
+            }
+
+
+            contador++;
+
+        } else if (contador == 1) {
+
+
+
+
+
         }
-        else if(operador.equals("*"))
-        {
-            resultado= n1*n2;
-        }
-        else if(operador.equals("/"))
-        {
-            resultado= n1/n2;
-        }
-        res.setText(resultado.toString());
     }
-
-    public void onClickSuma(View view)
-    {
-        operador="+";
-        onClickOperacionCapturaNumero1(view);
-    }
-    public void onClickResta(View view)
-    {
-        operador="-";
-        onClickOperacionCapturaNumero1(view);
-    }
-    public void onClickMultiplicacion(View view)
-    {
-        operador="*";
-        onClickOperacionCapturaNumero1(view);
-    }
-    public void onClickDivision(View view)
-    {
-        operador="/";
-        onClickOperacionCapturaNumero1(view);
-    }
-
-    public void onClickOperacionCapturaNumero1(View miView)
-    {
-        TextView res= (TextView) findViewById(R.id.txt_resultado) ;
-        n1 = Double.parseDouble(res.getText().toString());
-        res.setText("");
-    }
-
-    public void onClickBorrar(View miView)
-    {
-        n1=0.0;
-        n2=0.0;
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText("");
-    }
-
-    public void onClickBtn1(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "1");
-    }
-    public void onClickBtn2(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-       res.setText(res.getText() + "2");
-    }
-    public void onClickBtn3(View view)
-    {
-        TextView res= (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "3");
-    }
-    public void onClickBtn4(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "4");
-    }
-    public void onClickBtn5(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "5");
-    }
-    public void onClickBtn6(View view)
-    {
-        TextView res= (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "6");
-    }
-    public void onClickBtn7(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "7");
-    }
-    public void onClickBtn8(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "8");
-    }
-    public void onClickBtn99(View view)
-    {
-        TextView res= (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "9");
-    }
-    public void onClickBtn0(View view)
-    {
-        TextView res = (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + "0");
-    }
-    public void onClickBtnPunto(View view)
-    {
-        TextView res= (TextView) findViewById(R.id.txt_resultado) ;
-        res.setText(res.getText() + ".");
-        operador = (res.getText().toString());
-        if(operador.toString().equals(".")){
-            res.setText("");
-            res.setText(res.getText() + "0.");
-
-        }
-
-    }
-
 }
-
